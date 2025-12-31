25014d73f923334bf4559e1d395bda4d58b3062f
Basudev-Das25 <basudevd983@gmail.com>
2025-12-31 22:15:40 +0530

    Improved NLP model using class-weighted Logistic Regression

--- model/train.py
+++ model/train.py
@@ -45,20 +45,20 @@ X_train, X_test, y_train, y_test = train_test_split(
 )
 
 #Logistic Regression model
-model = LogisticRegression(max_iter=1000)
+model = LogisticRegression(max_iter=1000, class_weight="balanced")
 model.fit(X_train, y_train)
 
 #Prediction
 y_pred = model.predict(X_test)
 
 
-# print("\nClassification Report")
-# print(classification_report(
-#     y_test,
-#     y_pred,
-#     zero_division=0
-#     )
-# )
+print("\nClassification Report")
+print(classification_report(
+    y_test,
+    y_pred,
+    zero_division=0
+    )
+)
 
 # ===== STEP 9: NLP EXPLAINABILITY =====
 
